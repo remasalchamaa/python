15f6dc1dab59df34396519b2e6ef6129bfc1d05f
amirzaei1367 <amirzaei@gmu.edu>
2023-07-31 12:48:01 -0400

    quiz version 2

--- class/quiz project v1/main.py
+++ class/quiz project v1/main.py
@@ -1,5 +1,12 @@
 from data import question_bank
-from quiz import Quiz
+from quiz import Question
 
-quiz = Quiz(question_bank)
-quiz.start()
+question_bank_obj = [Question(question["question"], question["correct_answer"]) for question in question_bank]
+score = 0 
+for i, question in enumerate(question_bank_obj):
+    answer = input(f"Q:{i+1}/{len(question_bank_obj)}: {question.question} (True/False):")
+    if answer.lower() == question.answer.lower():
+        score += 1
+        print(f"Correct! Your score is {score}/{i+1}")
+    else:
+        print(f"Incorrect! Your score is {score}/{i+1}")
