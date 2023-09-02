6af22fc4d8834f84a557428cc4c572cc0ace111c
Ali Mirzaeian <56352498+amirzaei1367@users.noreply.github.com>
2023-09-02 14:04:14 -0400

    Create main.py

---
+++ homework/homework3/main.py
@@ -0,0 +1,19 @@
+# Homework 2.1 - Conditionals
+
+
+# Task 1: Date Validator
+
+
+# Task 2: Water State Determiner
+
+
+# Task 3: Print True
+A = False
+B = False
+C = False
+D = False
+
+if (A or B) and (C or D) and not (A and not D):
+  print("Boolean expression is True!")
+else:
+  print("Boolean expression is False.")
