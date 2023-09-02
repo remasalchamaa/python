4b9b0e8ac9046e5372ed88cfa9946ce80935ccd2
Ali Mirzaeian <56352498+amirzaei1367@users.noreply.github.com>
2023-09-02 13:57:48 -0400

    Create main.py

---
+++ homework/homework2/main.py
@@ -0,0 +1,22 @@
+# Task 1: Triple Double
+growing_number = 15
+
+
+# Task 2: Value Swap
+first_number = 5
+second_number = 8
+
+# Task 3: Five and Two & 3 & 7
+
+# Task 4: Assign New Value
+current_year = 0
+
+# Task 5: Input Birth Year
+birth_year = 0
+
+
+# Task 6: Calculate Current Age:
+
+
+
+# Task 7: Total Ticket Cost:
