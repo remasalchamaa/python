db037f6b06e4feeed4420c172f2f831ce4ac6233
Ali Mirzaeian <56352498+amirzaei1367@users.noreply.github.com>
2023-09-02 13:49:29 -0400

    Create main.py

---
+++ homework1/main.py
@@ -0,0 +1,18 @@
+# Homework 1.1
+
+# Numbers, Variables, Data Types, Input
+
+# Task 1: Print Sum
+print()
+
+# Task 2: Print Hours
+print()
+
+# Task 3: Repeating Greeting
+print()
+
+# Task 4: Convert string to float
+print(type("25.7"))
+
+# Task 5: Un-stringify
+print("12" + "4")
