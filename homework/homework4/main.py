6ec59927539136589df5178c637f3f7eb750207a
Ali Mirzaeian <56352498+amirzaei1367@users.noreply.github.com>
2023-10-07 21:18:29 -0400

    Update main.py

--- homework/homework4/main.py
+++ homework/homework4/main.py
@@ -1,1 +1,18 @@
+#problem 1
+class Line:
+
+    def __init__(self,coor1,coor2):
+        pass
+    
+    def distance(self):
+        pass
+
+    def slope(self):
+        pass
+
+#problem 2
+class Cylinder:
+  pass
+
+
 
