bd5ab836ac455709ff6a14b11fb1ce354a7b2938
Ali Mirzaeian <56352498+amirzaei1367@users.noreply.github.com>
2024-01-03 20:02:31 -0500

    Create left_right_iteration.py

---
+++ reeborg/left_right_iteration.py
@@ -0,0 +1,28 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def turn_right_m():
+    turn_right()
+    move()
+    turn_right()
+
+
+def turn_left_m():
+    turn_left()
+    move()
+    turn_left()
+
+
+for i in range(10):
+    if i % 2 == 0:
+        for j in range(9):
+            move()
+        turn_left_m()
+    else:
+        for j in range(9):
+            move()
+        if i != 9:
+            turn_right_m()
