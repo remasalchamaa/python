b0f28e06d72100e671df4f08d9d52fa0d7c9704e
Ali Mirzaeian <56352498+amirzaei1367@users.noreply.github.com>
2024-01-06 11:26:21 -0500

    Update diamond2.py

--- reeborg/diamond2.py
+++ reeborg/diamond2.py
@@ -1,32 +1,31 @@
 import time
-
 rec_width = 7
 rec_height = 4
 diam_width = 7
 symb = '*'
 
 
-def head_foot(diam_width, rep, symb):
+def head_foot(diam_width, rec_width, symb):
     for i in range(1,diam_width,2):
         line = " "*((diam_width-i)//2) + f"{symb}"*i + " "* ((diam_width-i)//2)
-        print(line*rep)
-        time.sleep(0.5)
+        print(line*rec_width)
+
 
     for i in range(diam_width,0,-2):
         line = " "*((diam_width-i)//2) + f"{symb}"*i + " "* ((diam_width-i)//2)
-        print(line*rep)
+        print(line*rec_width)
         time.sleep(0.5)
 
-def body(diam_width, rep, symb):
+def body(diam_width, rec_height, symb):
     for i in range(1,diam_width,2):
         line = " "*((diam_width-i)//2) + f"{symb}"*i + " "* ((diam_width-i)//2)
-        line2 = " " * diam_width * (rep-2)
+        line2 = " " * diam_width * (rec_width-2)
         print(line + line2 + line)
         time.sleep(0.5)
 
     for i in range(diam_width,0,-2):
         line = " "*((diam_width-i)//2) + f"{symb}"*i + " "* ((diam_width-i)//2)
-        line2 = " " * diam_width * (rep-2)
+        line2 = " " * diam_width * (rec_width-2)
         print(line + line2 + line)
         time.sleep(0.5)
     
